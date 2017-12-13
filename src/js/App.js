d01fd66377ee2fd07b268963f701dce000fb2091
edwinchen85 <edwinchen85@gmail.com>
2017-12-14 01:24:45 +0800

    Create and render Stars, Button and Answer components

--- src/js/App.js
+++ src/js/App.js
@@ -1,10 +1,37 @@
 import React from 'react';
 
+const Stars = (props) => {
+  return (
+    <div>
+      ...
+    </div>
+  );
+}
+
+const Button = (props) => {
+  return (
+    <div>
+      ...
+    </div>
+  );
+}
+
+const Answer = (props) => {
+  return (
+    <div>
+      ...
+    </div>
+  );
+}
+
 class Game extends React.Component {
   render() {
     return (
       <div>
         <h3>Play Nine</h3>
+        <Stars />
+        <Button />
+        <Answer />
       </div>
     );
   }
