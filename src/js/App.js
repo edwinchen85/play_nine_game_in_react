48a3fccce43a72ffd56eb9a5e80d0d9f0571aa21
edwinchen85 <edwinchen85@gmail.com>
2017-12-14 02:09:19 +0800

    Randomize number of stars from 1 to 9

--- src/js/App.js
+++ src/js/App.js
@@ -1,12 +1,15 @@
 import React from 'react';
 
 const Stars = (props) => {
+  const numberOfStars = 1 + Math.floor(Math.random() * 9);
+
+  let stars = [];
+  for(let i=0; i<numberOfStars; i++) {
+    stars.push(<i key={i} className="fa fa-star"></i>);
+  }
   return (
     <div className="col-sm-5">
-      <i className="fa fa-star"></i>
-      <i className="fa fa-star"></i>
-      <i className="fa fa-star"></i>
-      <i className="fa fa-star"></i>
+      {stars}
     </div>
   );
 }
